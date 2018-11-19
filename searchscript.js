79924efc2fd1a8bb9798d1eb7adacffa6700f023
Ajaybissick <Ajay.bissick@gmail.com>
2018-11-18 21:35:20 -0500

    Exercise three completed.

--- searchscript.js
+++ searchscript.js
@@ -1,13 +1,16 @@
 const httprequest = new XMLHttpREquest();
 const search = document.getElementById("searchfield")
+const def_result = document.getElementById("result")
 
 document.getElementById("searchbutton").addEventListener("click", function(){
     httprequest.open("GET", "request.php?q=$(search.value)", true)
     httprequest.send()
+    
     httprequest.onreadystatechange = function(){
+
         if (httprequest.readyState=== XMLHttpRequest.DONE){
             if(httprequest.status===200){
-                alert(httprequest.responseText)
+                def_result.innerHTML=httprequest.responseText
             }
         }
     }
