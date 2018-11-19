b8c7dee7330d5b10703a193ac1c15249da8f7aae
Ajaybissick <Ajay.bissick@gmail.com>
2018-11-18 22:11:49 -0500

    Final exercise updated and completed.

--- searchscript.js
+++ searchscript.js
@@ -1,5 +1,6 @@
 const httprequest = new XMLHttpREquest();
 const search = document.getElementById("searchfield")
+const searchall = document.getElementById("searchAlldefs")
 const def_result = document.getElementById("result")
 
 document.getElementById("searchbutton").addEventListener("click", function(){
@@ -15,3 +16,28 @@ document.getElementById("searchbutton").addEventListener("click", function(){
         }
     }
 });
+
+searchall.addEventListener("click", function(){
+    httprequest.open("GET", "request.php?q=$(search.value)", true)
+    httprequest.send()
+    
+    httprequest.onreadystatechange = function(){
+        if (httprequest.readyState=== XMLHttpRequest.DONE){
+            if(httprequest.status===200){
+                const response = httprequest.responseXML;
+                const definitions = response.getElementsByTagName("definition");
+                const dictionary = document.createElement("ol");
+                for(let i=0; i < definitions.length; i++){
+                    const word = document.createElement("li");
+                    const title = definitions[i].getAttribute('name');
+                    const author = definitions[i].getAttribute('author');
+                    const definition = definitions[i].innerHTML;
+                    word.innerHTML='<h3>${title}</h3><p>${definition}</p><p>${author}</p>';
+                    dictionary.appendChild(word);
+                }
+                def_result.appendChild(dictionary);
+            }
+        }
+    }
+});
+
